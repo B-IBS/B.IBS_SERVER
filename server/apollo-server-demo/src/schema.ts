0b69e13dda9535f85d889d2eefb2e45a6c1e73dc
Basile <basile.lamarque@epitech.eu>
2019-12-10 15:53:15 +0100

    [ADD] create the appollo server and connect the server to the data base

---
+++ server/apollo-server-demo/src/schema.ts
@@ -0,0 +1,8 @@
+import 'graphql-import-node';
+import * as typeDefs from './schema/schema.graphql';
+import { makeExecutableSchema } from 'graphql-tools';
+import resolvers from './schema/resolverMap';
+import { GraphQLSchema } from 'graphql';const schema: GraphQLSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});export default schema;
